74f49fb031460dd5c38c30e76852621c87086dac
marcoyel21 <marcoyel21@gmail.com>
2024-10-26 06:13:19 +0000

    fix

--- get_pdfs_selenium.py
+++ get_pdfs_selenium.py
@@ -12,6 +12,5 @@ import sys
 
 
 
-#for i in tqdm(range(0,10)):
-#    contract_files_upload_to_s3_paralel_selenium('contratos',limit = 5000, offset = 5000*i)
-contract_files_upload_to_s3_paralel_selenium(table='contratos',limit = 5000, offset = 5000,status  = 'archivo_historico', table_archivos = 'contratos_historicos')
+for i in tqdm(range(0,10)):
+    contract_files_upload_to_s3_paralel_selenium('contratos',limit = 5000, offset = 5000*i,status  = 'archivo_historico', table_archivos = 'contratos_historicos')
--- src/paralel.py
+++ src/paralel.py
@@ -136,10 +136,11 @@ def split_tasks_files_to_s3_selenium(table, limit, offset,status  = 'archivo_his
     logger.info(f'Created {tasks} tasks')
     return processes
 
+
 def contract_files_upload_to_s3_paralel(table,limit,offset):
     tasks_list = split_tasks_files_to_s3(table,limit,offset)
     execute_processes_list_in_batches(tasks_list,8)
 
 def contract_files_upload_to_s3_paralel_selenium(table,limit,offset,status, table_archivos):
     tasks_list = split_tasks_files_to_s3_selenium(table,limit,offset,status, table_archivos)
-    execute_processes_list_in_batches(tasks_list,1)
+    execute_processes_list_in_batches(tasks_list,8)
